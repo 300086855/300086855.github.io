3ed7e5d3c9d0f4b68f354da5390f2a0fea1fd919
Hnia Thao <mrhnia@hotmal.com>
2024-02-07 21:35:51 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,24 @@
+
+const hours = new Date().getHours(); // get the current hour
+
+
+const isMorning = hours >= 4 && hours < 12; // is it morning?
+const isAfternoon = hours >= 12 && hours < 17; // is it afternoon?
+const isEvening = hours >= 17 || hours < 4; // is it evening?
+
+
+const getMessage = () => {
+    
+    if (isMorning) return "Good Morning!"
+    
+    if (isAfternoon) return "Good Afternoon!"
+
+    if (isEvening) return "Good Evening!"
+
+    return ""
+}
+
+const message = getMessage()
+
+document.querySelector('#welcome h1').textContent = message
+document.querySelector('#welcome p').textContent = `The time is ${hours} hours`
